3b733a02bbc754cdf6cfe5eb6fee23b0275c2d1e
Anton Duppils <anton.duppils@gmail.com>
2024-01-01 16:14:00 +0100

    Initial work on platformer tutorial for Godot
    source material https://www.youtube.com/watch?v=S8lMTwSRoRg&list=PLrrZpXCbhjESqc7hF53BtgcT6FR_5vdOh&index=50&t=2832s

---
+++ Frog.gd
@@ -0,0 +1,75 @@
+extends CharacterBody2D
+
+var SPEED = 50
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var chase = false
+var dead = false
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
+func _physics_process(delta):
+	var anim = get_node("AnimatedSprite2D")
+	if anim.animation == "Death":
+		return
+	velocity.y += gravity + delta
+	if chase == true:
+		anim.play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		velocity.x = direction.x * SPEED
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		elif direction.x < 0:
+			get_node("AnimatedSprite2D").flip_h = false
+	else:
+		velocity.x = 0
+		anim.play("Idle")
+	
+	move_and_slide()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+		
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+
+
+func death():
+	dead = true
+	chase = false
+	Game.gold += 5
+	Utils.save_game()
+	call_deferred("_disable_collision")
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		print("frog hurt collision")
+		if not dead:
+			print("frog hurt")
+			body.bounce()
+			death()
+
+func _disable_collision():
+	get_node("CollisionShape2D").disabled = true
+	get_node("PlayerDeath/CollisionShape2D").disabled = true
+	get_node("PlayerCollision/CollisionShape2D").disabled = true
+	
+	var anim = get_node("AnimatedSprite2D")
+	anim.play("Death")
+	await anim.animation_finished
+	self.queue_free()
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		print("player hurt collision")
+		if not dead:
+			print("player hurt")
+			Game.player_hp -= 1
+			death()
