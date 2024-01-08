387e1b9f2e3979ae5efd6b43deadcdd668726444
Anton Duppils <anton.duppils@gmail.com>
2024-01-08 21:14:16 +0100

    Fix camera range and increase spawn rate for frogs

--- Enemy/Frog.gd
+++ Enemy/Frog.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var SPEED = 50
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") / 200
 var player
 var chase = false
 var dead = false
--- Enemy/Mobs.gd
+++ Enemy/Mobs.gd
@@ -6,5 +6,5 @@ func _on_timer_timeout():
 	var mob_tmp = Mob.instantiate()
 	var rng = RandomNumberGenerator.new()
 	var pos_x = rng.randi_range(10, 2000)
-	mob_tmp.position = Vector2(pos_x, 400)
+	mob_tmp.position = Vector2(pos_x, 10)
 	add_child(mob_tmp)
